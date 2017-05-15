0a1349e7048bc0f8df45847c7ab32c3a82f20f4e
Anushree Chandak <canushree1@gmail.com>
2017-05-14 22:05:33 -0400

    Renamed few variables
    Signed-off-by: Anushree Chandak <canushree1@gmail.com>

--- MailChimpTest/src/TeamMembers.java
+++ MailChimpTest/src/TeamMembers.java
@@ -7,20 +7,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class TeamMembers {
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");
-		
+		System.setProperty("webdriver.gecko.driver", "./geckodriver.exe");		
 		WebDriver driver = null;
 		FileWriter fileWriter = null;
 		
 		try {
-			driver = new FirefoxDriver();
-			fileWriter = new FileWriter("./output.csv");
-	
-			driver.navigate().to("https://mailchimp.com/");
+			driver = new FirefoxDriver(); 
+			fileWriter = new FileWriter("./output.csv"); 
+			
+	        // open the webpage and navigate to About page
+			driver.navigate().to("https://mailchimp.com/"); 
 	     	driver.findElement(By.linkText("About MailChimp")).click();
 	     	Thread.sleep(1000);
+	     	
+	     	// find names of all team members from leadership
 	     	String data = driver.findElement(By.cssSelector(".row.align-left.bio_wrapper")).getText();
-	     	String textStr[] = data.split("\\r\\n|\\n|\\r");
+	     	String nameList[] = data.split("\\r\\n|\\n|\\r");
 	     	
 	     	fileWriter.append("Name");
 	     	fileWriter.append(",");
@@ -29,19 +31,20 @@ public class TeamMembers {
 	     	fileWriter.append("Description");
 	     	fileWriter.append("\n");
 	     	
-	     	for (int i=0; i<textStr.length; i = i+2) {
-		     	driver.findElement(By.xpath("//*[contains(@class,'mb1') and text()='"+textStr[i]+"']")).click();
+	     	// Loop through all the team members and write their details to csv file
+	     	for (int i=0; i<nameList.length; i = i+2) {
+		     	driver.findElement(By.xpath("//*[contains(@class,'mb1') and text()='"+nameList[i]+"']")).click();
 		     	Thread.sleep(1000);
-		     	String data1 = driver.findElement(By.id("bio_view")).getText();
+		     	String description = driver.findElement(By.id("bio_view")).getText();
 		     	driver.findElement(By.cssSelector(".close_btn.icon-close-large")).click();
 		     	Thread.sleep(1000);
-	
-		     	String text[] = data1.split("\\r\\n|\\n|\\r");
-		     	fileWriter.append(text[0]);
+		     	
+		     	String temp[] = description.split("\\r\\n|\\n|\\r"); 
+		     	fileWriter.append(temp[0]); 
 		     	fileWriter.append(",");
-		     	fileWriter.append(text[1]);
+		     	fileWriter.append(temp[1]); 
 		     	fileWriter.append(",");
-		     	fileWriter.append("\"" + text[2].replaceAll("\"", "\"\"") + "\"");
+		     	fileWriter.append("\"" + temp[2].replaceAll("\"", "\"\"") + "\"");
 		     	fileWriter.append("\n");
 	     	}
 		}
